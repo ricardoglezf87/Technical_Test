94772f084ef150defe924fddd265c96ac29044ac
Ricardo Gonzalez Fabelo <rgonzalez@friwer.es>
2021-07-28 00:14:36 +0100

    Solucionar incidencia con clase inexitente

--- Technical_Test/Startup.cs
+++ Technical_Test/Startup.cs
@@ -27,13 +27,8 @@ namespace Technical_Test
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {            
-
-            services.Configure<ApplicationConfigurations>
-                (Configuration.GetSection("ApplicationConfigurations"));
-            
+        {                                    
             services.AddControllersWithViews();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
